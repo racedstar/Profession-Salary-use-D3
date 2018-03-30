ccdf931aef03335257ff0bc98045788deae6d8ae
racedstar <racedstar@gmail.com>
2018-03-30 16:22:10 +0800

    add transition

--- scripts/main.js
+++ scripts/main.js
@@ -3,39 +3,32 @@ $(document).ready(function(){
     var svgHeight = 400;
     var margin = 50;
     var data = getData('Recurring');
-    var yMax = salaryMax(data);
 
-    drawCharts(data, svgWidth, svgHeight, margin, yMax);
+    drawCharts(data, svgWidth, svgHeight, margin, false);
 
     $('#btnRecurring').click(function(){
         data = getData('Recurring');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
     $('#btnSecondary').click(function(){
         data = getData('Secondary');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
     $('#btnHighSchool').click(function(){
         data = getData('HighSchool');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
     $('#btnSpecialist').click(function(){
         data = getData('Specialist');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
     $('#btnUniversity').click(function(){
         data = getData('University');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
     $('#btnGraduateSchool').click(function(){
         data = getData('GraduateSchool');
-        yMax = salaryMax(data);
-        updateChart(data, svgWidth, svgHeight, margin, yMax);
+        drawCharts(data, svgWidth, svgHeight, margin, true);
     })
 })
 
@@ -81,18 +74,14 @@ var getData = function(type){
     return rawData;
 }
 
-//拿到薪水最高的值
-var salaryMax = function(data){
-    var yMax = d3.max(data, function(data){
-        return data.salary;
-    });
-    return yMax;
-}
-
 //(chartdata, svg寬度, svg高度, 間距)
-var drawCharts = function(rawData, svgWidth, svgHeight, margin, yMax){    
+var drawCharts = function(rawData, svgWidth, svgHeight, margin, ifUpdate){    
     svgWidth = svgWidth - margin;
     svgHeight = svgHeight - margin;    
+
+    var yMax = d3.max(rawData, function(data){
+        return data.salary;
+    });
 
     //算出x的位置
     var xScale = d3.scaleBand()
@@ -124,116 +113,63 @@ var drawCharts = function(rawData, svgWidth, svgHeight, margin, yMax){
                 });
     
     //在body內畫出svg
-    var svg = d3.select('#divCharts')
-        .append('svg')
+    var svg = d3.select('svg')        
         .attrs({
             width: svgWidth + margin,
             height: svgHeight + margin + 50,
             'style': 'padding-top: 50px; '
         });
 
-    //畫出長條圖
-    svg.selectAll('.bar')
-        .data(rawData)
-        .enter()
-        .append('rect')
-        .call(tip)
-        .classed('bar', true)
+    //畫出x,y軸
+    if(ifUpdate == false){
+        svg.append('g')
         .attrs({
-            x: function(data){
-                return xScale(data.profession);
-            },
-            y: function(data){
-                return svgHeight - yScale(data.salary);
-            },
-            width: xScale.bandwidth(),
-            //width: 0,
-            height: function(data){
-                return yScale(data.salary);
-            }
-        })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-     
-    //畫出X軸
-    svg.append('g')
-        .attrs({
-            'transform': 'translate(0,' + (svgHeight) + ')',
-            'class': 'xAxis'
+            'class': 'xAxis',
+            'transform': 'translate(0,' + (svgHeight) + ')'        
         })
         .call(xAxis);
     
-    //畫出Y軸
-    svg.append('g') 
-        .attrs({
-            'class': 'yAxis',
-            'transform': 'translate(' + (margin - 10) + ', ' + 0 + ')'
-        })       
-        .call(yAxis);
-}
-
-
-
-//(chartdata, svg寬度, svg高度, 間距)
-var updateChart = function(rawData, svgWidth, svgHeight, margin, yMax){    
-    svgWidth = svgWidth - margin;
-    svgHeight = svgHeight - margin;
-
-    //算出x的位置
-    var xScale = d3.scaleBand()
-                    .domain(rawData.map(function(d){
-                        return d.profession;
-                    }))
-                    .range([margin, svgWidth])
-                    .paddingInner(0.3);
+        //畫出Y軸
+        svg.append('g') 
+            .attrs({
+                'class': 'yAxis',
+                'transform': 'translate(' + (margin - 10) + ', ' + 0 + ')'
+            })
+            .call(yAxis);
+    }
     
-    //算出所有薪水的長條圖                    
-    var yScale = d3.scaleLinear()
-                    .domain([0, yMax])
-                    .range([0, svgHeight]);
+    svg.selectAll('.xAxis').transition().duration(1000).call(xAxis);
+    svg.selectAll('.yAxis').transition().duration(1000).call(yAxis);
 
-    //給yAxis用
-    var yScaleAxis = d3.scaleLinear()
-                        .domain([0, yMax])                    
-                        .range([svgHeight, 0])
-
-    var xAxis = d3.axisBottom(xScale);
-    var yAxis = d3.axisLeft(yScaleAxis);
-
-    //tooltip
-    var tip = d3.tip()
-                .attr('class', 'd3-tip')
-                .offset([-10, 0])
-                .html(function(d){
-                    return "<strong>Profession: </strong><sapn style=color: blue>" + d.profession + "</span><br />" + "<strong>Salary: </strong><span style='color: red'>" + d.salary + '</span>'
-                });
-    
     //畫出長條圖
-    var bars = d3.select('svg');
-    
-        bars.selectAll('.bar')
-        .remove()
+    svg.selectAll('.bar')
         .exit()
         .data(rawData)
-        .enter()   
+        .enter()
         .append('rect')
+        .classed('bar', true)
         .call(tip)
-        .transition()
-        .duration(2500)
         .attrs({
-            'class': 'bar',
             x: function(data){
                 return xScale(data.profession);
             },
-            y: function(data){                
-                return svgHeight - yScale(data.salary);
-            },
+            y: 0,
             width: xScale.bandwidth(),
-            // width: 0,
-            height: function(data){
-                return yScale(data.salary);
-            }
-        })            
+            height: 0
+        })
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
+        
+    svg.selectAll('.bar')
+        .transition()
+        .duration(1000)
+        .attrs({
+            y: function(data){
+                return svgHeight - yScale(data.salary);
+            },
+            height: function(data){                
+                return yScale(data.salary);
+                
+            }
+        });
 }
